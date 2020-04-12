8738867f79fe5676696207b06980245829d103d9
RuiSuuun <58921284+RuiSuuun@users.noreply.github.com>
2020-04-12 17:57:31 -0400

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <div>You are logged in!</div>
+                    <div>Click "Tasks" in the Navigation Bar to start with you list!</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
